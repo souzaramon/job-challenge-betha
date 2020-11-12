ede7c447684ec4e920a0a64a1f9fc5730e88ff56
souzaramon <ramon_souza@hotmail.com>
2020-11-12 03:43:58 -0300

    fix: general fixes

--- packages/app/src/pages/Signup/Signup.ctrl.ts
+++ packages/app/src/pages/Signup/Signup.ctrl.ts
@@ -1,5 +1,18 @@
 function Controller($scope: any) {
-  $scope.values = {};
+  $scope.values = {
+    name: '',
+    email: '',
+    password: '',
+    terms: false
+  };
+
+  $scope.loading = false;
+
+  // computed-property
+  Object.defineProperty($scope, 'isFieldsEmpty', {
+    get: () => !Object.values($scope.values).every(v => !!v && v !== "")
+  })
+
   $scope.handleSubmit = (e: any) => {
     console.log($scope.values)
   }
